4927938e934062c74246de74624478b6e9eb02a7
Darren Cauthon <darren@cauthon.com>
2012-08-16 19:09:42 -0500

    Add a second situation.

--- xyz/xyz_service_spec.rb
+++ xyz/xyz_service_spec.rb
@@ -7,25 +7,50 @@ require 'date'
 
 describe XYZService do
 
-  before do
-    messages = {
-      :publish_on => Date.new(2012, 3, 14),
-      :xyz_category_prefix => 'abc',
-      :kind => 'unicorn',
-      :personal? => false,
-      :id => 1337,
-      :title => 'magic & superglue'
-    }
-    target = Hashie::Mash.new messages
+  describe "one situation" do
+    before do
+      messages = {
+        :publish_on => Date.new(2012, 3, 14),
+        :xyz_category_prefix => 'abc',
+        :kind => 'unicorn',
+        :personal? => false,
+        :id => 1337,
+        :title => 'magic & superglue'
+      }
+      target = Hashie::Mash.new messages
 
-    XYZService.expects(:rand).with(10000).returns(4321)
-    Digest::SHA1.expects(:hexdigest).with('4321').returns('abcdefghijklmnop')
-    
-    @subject = XYZService.xyz_filename(target)
+      XYZService.expects(:rand).with(10000).returns(4321)
+      Digest::SHA1.expects(:hexdigest).with('4321').returns('abcdefghijklmnop')
+      
+      @subject = XYZService.xyz_filename(target)
+    end
+
+    it 'works' do
+      @subject.must_equal '14abcunicorn_1337_abcdefgh_magicsuper.jpg'
+    end
   end
 
-  it 'works' do
-    @subject.must_equal '14abcunicorn_1337_abcdefgh_magicsuper.jpg'
+  describe "another situation" do
+    before do
+      messages = {
+        :publish_on => Date.new(2013, 7, 31),
+        :xyz_category_prefix => 'get',
+        :kind => 'applesauce',
+        :personal? => true,
+        :id => 6512,
+        :title => 'NOW IS THE TIME FOR ALL GOOD MEN'
+      }
+      target = Hashie::Mash.new messages
+
+      XYZService.expects(:rand).with(10000).returns(7423)
+      Digest::SHA1.expects(:hexdigest).with('7423').returns('rocksteady')
+      
+      @subject = XYZService.xyz_filename(target)
+    end
+
+    it 'works' do
+      @subject.must_equal '31getapplesauce_000_6512_rockstea_nowistheti.jpg'
+    end
   end
 
 end
